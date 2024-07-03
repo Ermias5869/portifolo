7d78cedf29504726f614c4c44c3d2605cbc2e34e
tech nerd <yeabnoah5@gmail.com>
2024-07-03 18:18:41 +0300

    finished portifolio

--- app/(footer)/components/footer.tsx
+++ app/(footer)/components/footer.tsx
@@ -3,24 +3,24 @@ import { motion } from "framer-motion";
 
 function Footer() {
   return (
-    <div className=" flex justify-center items-center mt-20">
+    <div className="flex justify-center items-center mt-20">
       <div>
-        <h2 className=" underline text-xl text-center my-5 font-semibold text-green-500">
+        <h2 className="underline text-xl text-center my-5 font-semibold text-green-500">
           2024 made By TechNerd
         </h2>
-        <p className=" text-center hover:text-green-500 selection:bg-green-500 text-sm mx-5 w-[90%] m-auto lg:w-[60%] text-green-500  selection:text-black text-balance  justify-center lg:mx-auto">
+        <p className="text-center hover:text-green-500 selection:bg-green-500 text-sm mx-5 w-[90%] m-auto lg:w-[60%] text-green-500 selection:text-black text-balance justify-center lg:mx-auto">
           {" "}
-          I'd love to hear from you! Whether you have a project in mind, need
-          help with development, or just want to connect and chat about
+          I&apos;d love to hear from you! Whether you have a project in mind,
+          need help with development, or just want to connect and chat about
           technology, feel free to reach out. You can email me at
-          <span className=" underline font-bold px-2">
+          <span className="underline font-bold px-2">
             yeabnoah5@gmail.com
           </span>{" "}
           or connect with me on telegram{" "}
-          <span className=" underline font-bold px-2">@technerd345.</span>
-          Let's collaborate and create something amazing together!
+          <span className="underline font-bold px-2">@technerd345.</span>
+          Let&apos;s collaborate and create something amazing together!
         </p>
-        <h2 className=" underline text-sm text-center my-10 font-semibold text-green-500">
+        <h2 className="underline text-sm text-center my-10 font-semibold text-green-500">
           2024 made By TechNerd
         </h2>
       </div>
--- app/(home)/components/hero.tsx
+++ app/(home)/components/hero.tsx
@@ -3,26 +3,27 @@ import React from "react";
 function Hero() {
   return (
     <div className="">
-      <div className="flex justify-center items-center ">
+      <div className="flex justify-center items-center">
         <div className="">
-          <p className=" text-center hover:text-green-500 selection:bg-green-500 text-sm mx-5 w-[90%] m-auto lg:w-[60%] text-green-500  selection:text-black text-balance  justify-center lg:mx-auto">
+          <p className="text-center hover:text-green-500 selection:bg-green-500 text-sm mx-5 w-[90%] m-auto lg:w-[60%] text-green-500 selection:text-black text-balance justify-center lg:mx-auto">
             {" "}
-            👋 Hello! I'm Yeabsra 💇‍♂️, a web and mobile developer from Ethiopia.
-            I taught myself how to build websites and apps, and I enjoy using
-            technology to solve problems and bring ideas to life. I'm meticulous
-            in my work, paying close attention to small details and finding
-            creative solutions when challenges arise. When I'm not coding, I'm
-            always exploring new technologies and learning something new.
+            👋 Hello! I&apos;m Yeabsra 💇‍♂️, a web and mobile developer from
+            Ethiopia. I taught myself how to build websites and apps, and I
+            enjoy using technology to solve problems and bring ideas to life.
+            I&apos;m meticulous in my work, paying close attention to small
+            details and finding creative solutions when challenges arise. When
+            I&apos;m not coding, I&apos;m always exploring new technologies and
+            learning something new.
           </p>
 
-          <p className=" selection:bg-green-500 hover:text-green-500 text-sm mx-5 m-auto w-[90%] lg:w-[60%] text-green-500  lg:mx-auto selection:text-black text-balance text-center mt-5">
+          <p className="selection:bg-green-500 hover:text-green-500 text-sm mx-5 m-auto w-[90%] lg:w-[60%] text-green-500 lg:mx-auto selection:text-black text-balance text-center mt-5">
             {" "}
-            I'm passionate about building projects and sharing them with others.
-            I thrive on collaboration and love working with others to create
-            innovative solutions and make cool stuff happen. Whether it's a
-            complex web application or a sleek mobile app, I'm excited to bring
-            your ideas to life. Let's connect and work together to achieve
-            something amazing!
+            I&apos;m passionate about building projects and sharing them with
+            others. I thrive on collaboration and love working with others to
+            create innovative solutions and make cool stuff happen. Whether
+            it&apos;s a complex web application or a sleek mobile app, I&apos;m
+            excited to bring your ideas to life. Let&apos;s connect and work
+            together to achieve something amazing!
           </p>
         </div>
       </div>
